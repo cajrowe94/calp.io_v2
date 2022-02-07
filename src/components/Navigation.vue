ca5a0f141ddae0471d23c87f036f16142fea9321
Caleb Rowe <cajrowe@iu.edu>
2022-02-06 20:18:38 -0500

    scss, layout component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+	<nav class="navigation">
+		<div class="navigation-spacer"></div>
+		<div class="content">
+			<a
+				href="/"
+				class="navigation-logo"
+			>
+			cr
+			</a>
+		</div>
+	</nav>
+</template>
+
+<script>
+	export default {
+		name: 'Navigation',
+	}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="view">
+    <!-- Navigation -->
+    <Navigation />
+
+    <!-- Main Layout -->
+    <Layout>
+      <!-- Left slot -->
+      <template v-slot:layoutLeft>
+        <h1>Left slot</h1>
+      </template>
+
+      <!-- Center slot -->
+      <template v-slot:layoutCenter>
+        <h1>Center slot</h1>
+      </template>
+
+      <!-- Left slot -->
+      <template v-slot:layoutRight>
+        <h1>Right slot</h1>
+      </template>
+    </Layout>
+  </div>
+</template>
+
+<script>
+  import Navigation from '../components/Navigation.vue';
+  import Layout from '../components/Layout.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      Navigation,
+      Layout,
+    }
+  }
+</script>
