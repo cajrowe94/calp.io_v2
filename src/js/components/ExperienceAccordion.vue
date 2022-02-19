0c931c0e932fbe79e0e669f01717dd8cc0eab76d
Caleb Rowe <cajrowe@iu.edu>
2022-02-18 23:02:28 -0500

    accordion

--- src/js/components/ExperienceAccordion.vue
+++ src/js/components/ExperienceAccordion.vue
@@ -1,8 +1,77 @@
 <template>
 	<div class="accordion-container">
 		<div class="accordion-items">
+			<!-- HDMZ -->
 			<div class="accordion-item active">
-				
+				<div class="accordion-item-header">
+					<div>
+						<p class="title">
+							<span class="job-title">Frontend Developer</span>
+							<span class="at">&commat;</span>
+							<span class="company-name">HDMZ</span>
+						</p>
+						<p class="dates">Feb 2021 - Present : Remote</p>
+					</div>
+					<div class="accordion-arrow">
+						<img src="../../assets/icons/accordion_arrow.svg">
+					</div>
+				</div>
+				<div class="accordion-item-body">
+					<ul class="white">
+						<li>List item</li>
+						<li>List item</li>
+						<li>List item</li>
+					</ul>
+				</div>
+			</div>
+			<!-- myhELO -->
+			<div class="accordion-item">
+				<div class="accordion-item-header">
+					<div>
+						<p class="title">
+							<span class="job-title">Software Engineer</span>
+							<span class="at">&commat;</span>
+							<span class="company-name">myhELO</span>
+						</p>
+						<p class="dates">Jan 2020 - Feb 2021 : Fishers, IN</p>
+					</div>
+					<div class="accordion-arrow">
+						<img src="../../assets/icons/accordion_arrow.svg">
+					</div>
+				</div>
+				<div class="accordion-item-body">
+					<ul class="white">
+						<li>List item</li>
+						<li>List item</li>
+						<li>List item</li>
+					</ul>
+				</div>
+			</div>
+			<!-- USWS -->
+			<div class="accordion-item">
+				<div class="accordion-item-header">
+					<div>
+						<p class="title">
+							<span class="job-title">Web Developer</span>
+							<span class="at">&commat;</span>
+							<span class="company-name">US Water Systems</span>
+						</p>
+						<p class="dates">April 2019 - Jan 2020 : Indianapolis, IN</p>
+					</div>
+					<div class="accordion-arrow">
+						<img src="../../assets/icons/accordion_arrow.svg">
+					</div>
+				</div>
+				<div class="accordion-item-body">
+					<ul class="white">
+						<li>List item</li>
+						<li>List item</li>
+						<li>List item</li>
+						<li>List item</li>
+						<li>List item</li>
+						<li>List item</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -15,6 +84,27 @@
 			return {
 
 			}
+		},
+		mounted() {
+			// add a click listener for each accordion item
+			var items = document.querySelectorAll('.accordion-item');
+			console.log(items);
+
+			items.forEach(item => {
+				item.addEventListener('click', () => {
+					// first, remove all active items
+					if (!item.classList.contains('active')) {
+						items.forEach(item => {
+							if (item.classList.contains('active')) {
+								item.classList.remove('active');
+							}
+						});
+
+						// now add the active class to clicked item
+						item.classList.add('active');
+					}
+				})
+			})
 		}
 	}
 </script>
